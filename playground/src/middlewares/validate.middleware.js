453ed793be33b3d29b201773acce22abe4e3d731
CodeWithZain10 <zainzahid10116@gmail.com>
2026-07-30 00:08:19 +0500

    fix: resolve import conflict for ValidationError in validate middleware

--- playground/src/middlewares/validate.middleware.js
+++ playground/src/middlewares/validate.middleware.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { ValidationError } from '../utils/errors/AppError.js'
-=======
-import { validationError } from '../utils/errors/AppError.js'
->>>>>>> 95507b70c05d8a1d6764016fcfd3aadec2cd03a9
 
 const validateMiddleware = (schema) => {
     return (req, res, next) => {
